8f3f6d646360509bd0e78a2cc65ad6a9968b59a9
Tobiáš Radkovský <toby.radkovsky@gmail.com>
2025-01-03 20:42:09 +0100

    fixed google login, added login support

--- website/views.py
+++ website/views.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask_login import current_user
 
 view = Blueprint('view', __name__)
 
 @view.route('/')
 def home():
-    return render_template('base.html')
+    if current_user.is_authenticated:
+
+        user_id = request.args.get("user_id", current_user.id)
+        name = request.args.get("name", current_user.name)
+
+        return render_template('home_logged_in.html', user_id=user_id, name=name)
+    else:
+
+        return render_template('home_anonym.html')
