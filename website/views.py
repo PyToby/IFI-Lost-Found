ce21c67c50d93c5539ecba635ec9586e5d528caf
Tobiáš Radkovský <toby.radkovsky@gmail.com>
2025-01-04 13:33:37 +0100

    added pfp to mobile

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 view = Blueprint('view', __name__)
 
@@ -12,3 +12,8 @@ def home():
             return render_template('home.html', user_id=user_id, name=name, pfp=pfp, current_user=current_user)
         else:
             return render_template('home.html', current_user=current_user)
+
+@login_required
+@view.route('/profil')
+def profil():
+    return render_template('profile.html', name=current_user.name, pfp=current_user.pfp)
