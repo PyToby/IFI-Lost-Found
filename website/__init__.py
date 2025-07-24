148f83380ac6a197220035488a51f1c0369528e0
Tobiáš Radkovský <toby.radkovsky@gmail.com>
2025-07-24 11:46:00 +0200

    moved logging config to __init__.py, fixed database creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,23 @@
-from flask import Flask,session
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from oauthlib.oauth2 import WebApplicationClient
 import os
+import logging
 
+# --- Logging Setup ---
+LOG_DIR = os.path.join(os.path.dirname(__file__), 'logs')
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = os.path.join(LOG_DIR, 'app.log')
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+)
+
+# --- Flask App Setup ---
 db = SQLAlchemy()
 DB_NAME = 'database.db'
 
@@ -13,8 +27,11 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'mam_chut_te_zabit_ale_uz_to_prechazi_mas_stesti'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'mam_chut_te_zabit_ale_uz_to_prechazi_mas_stesti__to_je_dobre_Ondro_:)'
+
+    # Use instance folder for database
+    db_path = os.path.join(app.instance_path, DB_NAME)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -33,7 +50,8 @@ def create_app():
     return app
 
 def create_database(app):
-    if not os.path.exists('../../instance/' + DB_NAME):
+    db_path = os.path.join(app.instance_path, DB_NAME)
+    if not os.path.exists(db_path):
         with app.app_context():
             db.create_all()
         print('Created database!')
