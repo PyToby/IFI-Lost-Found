9a1f42200756ccfb90a4c6a1c86f8f4ceb1315fd
Ondřej Kvido Suchý <onkvisu@centrum.cz>
2026-03-08 18:20:10 +0100

    Bug fixes

--- website/views.py
+++ website/views.py
@@ -66,11 +66,50 @@ test_items=[
         "date":"31.2. 1969"
     },
     {
+        "image":"https://upload.wikimedia.org/wikipedia/en/8/8c/Woods_4_Green_Album_Cover_Art.jpg",
+        "title":"Death is not the end",
+        "info":"It's a new experience\n--DSBM",
+        "date":"11.12. 2010"},
+    {
+        "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/ApplePencilPro.png/640px-ApplePencilPro.png",
+        "title":"Tužka",
+        "info":"Značky Jablko. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla pulvinar eleifend sem. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc dapibus tortor vel mi dapibus sollicitudin. Aenean vel massa quis mauris vehicula lacinia. Nulla est. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Integer imperdiet lectus quis justo. Sed convallis magna eu sem. Praesent vitae arcu tempor neque lacinia pretium. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Integer vulputate sem a nibh rutrum consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.",
+        "date":"31.2. 1969"
+    },
+    {
         "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Tape_Measure_Massband_10_cm.svg/640px-Tape_Measure_Massband_10_cm.svg.png",
         "title":"Metr",
-        "info":"""POSLEDNÍ POLOŽKA!!!!!. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla pulvinar eleifend sem. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc dapibus tortor vel mi dapibus sollicitudin. Aenean vel massa quis mauris vehicula lacinia. Nulla est. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Integer imperdiet lectus quis justo. Sed convallis magna eu sem. Praesent vitae arcu tempor neque lacinia pretium. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Integer vulputate sem a nibh rutrum consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.""",
+        "info":"""Mé délky. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla pulvinar eleifend sem. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc dapibus tortor vel mi dapibus sollicitudin. Aenean vel massa quis mauris vehicula lacinia. Nulla est. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Integer imperdiet lectus quis justo. Sed convallis magna eu sem. Praesent vitae arcu tempor neque lacinia pretium. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Integer vulputate sem a nibh rutrum consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.""",
         "date":"-4.13. 2099"
-    }
+    },
+    {
+        "image":"https://upload.wikimedia.org/wikipedia/en/8/8c/Woods_4_Green_Album_Cover_Art.jpg",
+        "title":"Death is not the end",
+        "info":"It's a new experience\n--DSBM",
+        "date":"11.12. 2010"},
+    {
+        "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/ApplePencilPro.png/640px-ApplePencilPro.png",
+        "title":"Tužka",
+        "info":"Značky Jablko. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla pulvinar eleifend sem. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc dapibus tortor vel mi dapibus sollicitudin. Aenean vel massa quis mauris vehicula lacinia. Nulla est. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Integer imperdiet lectus quis justo. Sed convallis magna eu sem. Praesent vitae arcu tempor neque lacinia pretium. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Integer vulputate sem a nibh rutrum consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.",
+        "date":"31.2. 1969"
+    },
+    {
+        "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Tape_Measure_Massband_10_cm.svg/640px-Tape_Measure_Massband_10_cm.svg.png",
+        "title":"Metr",
+        "info":"""Mé délky. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla pulvinar eleifend sem. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc dapibus tortor vel mi dapibus sollicitudin. Aenean vel massa quis mauris vehicula lacinia. Nulla est. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Integer imperdiet lectus quis justo. Sed convallis magna eu sem. Praesent vitae arcu tempor neque lacinia pretium. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Integer vulputate sem a nibh rutrum consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.""",
+        "date":"-4.13. 2099"
+    },
+    {
+        "image":"https://upload.wikimedia.org/wikipedia/en/8/8c/Woods_4_Green_Album_Cover_Art.jpg",
+        "title":"Death is not the end",
+        "info":"It's a new experience\n--DSBM",
+        "date":"11.12. 2010"},
+    {
+        "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/ApplePencilPro.png/640px-ApplePencilPro.png",
+        "title":"(posledni)Tužka",
+        "info":"Značky Jablko. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla pulvinar eleifend sem. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Nunc dapibus tortor vel mi dapibus sollicitudin. Aenean vel massa quis mauris vehicula lacinia. Nulla est. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Integer imperdiet lectus quis justo. Sed convallis magna eu sem. Praesent vitae arcu tempor neque lacinia pretium. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Integer vulputate sem a nibh rutrum consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.",
+        "date":"31.2. 1969"
+    },
         ]
 
 ITEMS_PER_RELOAD = 7
@@ -99,18 +138,16 @@ def profil():
     pfp = request.args.get("pfp", current_user.pfp)
     return render_template('profile.html', name=name, pfp=pfp, user_id=user_id, current_user=current_user)
 
-<<<<<<< HEAD
 @view.route('/background_process_test')
 def background_process_test():
     print("Hello")
     return render_template('profile.html')
-=======
 
 @view.route('/info')
 def info():
     return render_template('info.html', current_user=current_user)
 
-@view.route("load_next_items")
+@view.route("/load_next_items")
 def load_next_items():
     session["test_items_index"] += ITEMS_PER_RELOAD
     test_items_index = session["test_items_index"]
@@ -119,4 +156,3 @@ def load_next_items():
         more_items_available=False
 
     return render_template("preview_list.html",test_items=test_items[test_items_index:test_items_index+ITEMS_PER_RELOAD],more_items_available=more_items_available)
->>>>>>> 4aa01e305e46e24f842bdcaf07d6510f4f09bb56
